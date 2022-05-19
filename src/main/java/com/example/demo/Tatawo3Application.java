9f782d10f1d3252867c9a9e4edcdc7e3fa7cb47d
Souhaib Djaballah <souhaib.djab@gmail.com>
2022-05-19 10:35:04 +0100

    Initial commit
    This is the initial spring boot project.

---
+++ src/main/java/com/example/demo/Tatawo3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tatawo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tatawo3Application.class, args);
+	}
+
+}
